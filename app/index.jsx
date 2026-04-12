17659db01547c3a2f105c19d3d6ffcdf47902dc8
Rommel Dijon <bartmilhausky@gmail.com>
2026-04-12 16:39:08 -0700

    Updated texts and stylesheet

--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,34 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image, LogBox } from 'react-native'
 import React from 'react'
+import Logo from '../assets/img/logo.png'
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <Image source={Logo} style={styles.img} />
+      <Text style={styles.title}>The Number 1</Text>
+      <Text style={{ marginTop: 10, marginBottom: 30}}>
+        Reading List App
+      </Text>
     </View>
   )
 }
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  img: {
+    marginVertical: 20,
+    maxWidth: 100,
+    height: 100,
+  },
+})
