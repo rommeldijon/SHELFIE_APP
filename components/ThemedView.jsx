27abe9d64084e5aec180be6b0e7c70fe6fdc9f6f
Rommel Dijon <144735715+rommeldijon@users.noreply.github.com>
2026-04-16 09:03:31 -0700

    Fix ThemedView component to render themed container

--- components/ThemedView.jsx
+++ components/ThemedView.jsx
@@ -1,51 +1,11 @@
-import { StyleSheet } from 'react-native'
-import { Link } from 'expo-router'
+import { View, useColorScheme } from 'react-native'
+import { Colors } from '../constants/Colors'
 
-import ThemedView from "../components/ThemedView"
-import ThemedText from "../components/ThemedText"
-import ThemedLogo from "../components/ThemedLogo"
-import Spacer from "../components/Spacer"
+const ThemedView = ({ style, ...props }) => {
+  const colorScheme = useColorScheme()
+  const theme = Colors[colorScheme] ?? Colors.light
 
-const Home = () => {
-  return (
-    <ThemedView style={styles.container}>
-      <ThemedLogo />
-      <Spacer />
-
-      <ThemedText style={styles.title} title={true}>The Number 1</ThemedText>
-
-      <ThemedText style={{ marginTop: 10, marginBottom: 30 }}>
-        Reading List App
-      </ThemedText>
-
-      <Link href="/about" style={styles.link}>
-        <ThemedText>About Page</ThemedText>
-      </Link>
-
-      <Link href="/contact" style={styles.link}>
-        <ThemedText>Contact Page</ThemedText>
-      </Link>
-    </ThemedView>
-  )
+  return <View style={[{ backgroundColor: theme.background }, style]} {...props} />
 }
 
-export default Home
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  img: {
-    marginVertical: 20
-  },
-  title: {
-    fontWeight: 'bold',
-    fontSize: 18,
-  },
-  link: {
-    marginVertical: 10,
-    borderBottomWidth: 1
-  }
-})
+export default ThemedView
