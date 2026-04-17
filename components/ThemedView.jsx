63a68d819b565cc1f034b170a42567d561ce8d90
Rommel Dijon <rdijon@ymail.com>
2026-04-17 13:47:02 -0700

    added variation of SafeAreaView using useSafeAreaInsets-less janky when moving to different screens

--- components/ThemedView.jsx
+++ components/ThemedView.jsx
@@ -1,11 +1,31 @@
-import { View, useColorScheme } from 'react-native'
+import { SafeAreaView, View, useColorScheme } from 'react-native'
 import { Colors } from '../constants/Colors'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
-const ThemedView = ({ style, ...props }) => {
+const ThemedView = ({ style, safe = false, ...props }) => {
   const colorScheme = useColorScheme()
   const theme = Colors[colorScheme] ?? Colors.light
 
-  return <View style={[{ backgroundColor: theme.background }, style]} {...props} />
+  if (!safe) return ( 
+    <View 
+      style={[{ backgroundColor: theme.background }, style]} 
+      {...props} 
+    />
+  )
+
+  const insets = useSafeAreaInsets()
+
+  return ( 
+    <View 
+      style={[{ backgroundColor: theme.background, 
+      paddingTop: insets.top,
+      paddingBottom: insets.bottom,
+      paddingLeft: insets.left,
+      }, 
+        style]} 
+      {...props} 
+    />
+  )
 }
 
 export default ThemedView
