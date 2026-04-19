eca800b879f779df15c40ad5405e79038e424857
Rommel Dijon <bartmilhausky@gmail.com>
2026-04-19 09:19:26 -0700

    added comments for safe condition-instead of using SafeAreaView

--- components/ThemedView.jsx
+++ components/ThemedView.jsx
@@ -5,14 +5,16 @@ import { useSafeAreaInsets } from 'react-native-safe-area-context'
 const ThemedView = ({ style, safe = false, ...props }) => {
   const colorScheme = useColorScheme()
   const theme = Colors[colorScheme] ?? Colors.light
-
+  //For better functionality, instead of using a SafeAreaView, 
+  // we can just use a regular View and add padding to it to account for the safe area insets. This is because the SafeAreaView component can cause issues with certain layouts and can be difficult to work with in some cases.
+  //when safe is false, we can just return a regular view with the background color set to the theme's background color
   if (!safe) return ( 
     <View 
       style={[{ backgroundColor: theme.background }, style]} 
       {...props} 
     />
   )
-
+  //when safe is true, we need to add padding to the view to account for the safe area insets
   const insets = useSafeAreaInsets()
 
   return ( 
