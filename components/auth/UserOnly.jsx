b3d0068928546b696e0f26ae6d420e7dddf0dccf
Rommel Dijon <rdijon@ymail.com>
2026-04-21 09:45:17 -0700

    created user and guest routes

---
+++ components/auth/UserOnly.jsx
@@ -0,0 +1,26 @@
+import { useUser } from '../../hooks/useUser'
+import { useRouter } from 'expo-router'
+import { useEffect } from 'react'
+import { Text } from 'react-native'
+
+const UserOnly = ({ children }) => {
+  const { user, authChecked } = useUser()
+  const router = useRouter()
+  
+  useEffect(() => {
+    if (authChecked && user === null) {
+      router.replace("/login")
+    }
+  }, [user, authChecked]) //everytime these change, redirect to login if user becomes null after auth is checked
+
+  // show loader while we wait for auth to be checked, or while redirecting if user becomes null
+  if (!authChecked || !user) {
+    return (
+      <Text>Loading</Text>
+    )
+  }
+  
+  return children
+}
+
+export default UserOnly
