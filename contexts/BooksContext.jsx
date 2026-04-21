fe99c3e37981fd02cde75f37de19cc1656f4abe0
Rommel Dijon <rdijon@ymail.com>
2026-04-21 13:29:31 -0700

    added functionality of creating new records

--- contexts/BooksContext.jsx
+++ contexts/BooksContext.jsx
@@ -1,4 +1,7 @@
 import { createContext, useState } from "react"
+import { databases } from "../lib/appwrite"
+import { ID, Permission, Role } from "react-native-appwrite"
+import { useUser } from "../hooks/useUser"
 
 const DATABASE_ID = "69e7bf17003e06dc32c2"
 const COLLECTION_ID = "books"
@@ -7,6 +10,7 @@ export const BooksContext = createContext()
 
 export function BooksProvider({children}) {
   const [books, setBooks] = useState([])
+  const { user } = useUser()
 
   async function fetchBooks() {
     try {
@@ -18,9 +22,7 @@ export function BooksProvider({children}) {
 
   async function fetchBookById(id) {
     try {
-
-  
-      return response 
+      
     } catch (error) {
       console.log(error.message)
     }
@@ -28,7 +30,17 @@ export function BooksProvider({children}) {
 
   async function createBook(data) {
     try {
-      
+      await databases.createDocument(
+        DATABASE_ID,
+        COLLECTION_ID,
+        ID.unique(),
+        {...data, userId: user.$id},
+        [
+          Permission.read(Role.user(user.$id)),
+          Permission.update(Role.user(user.$id)),
+          Permission.delete(Role.user(user.$id)),
+        ]
+      )
     } catch (error) {
       console.log(error.message)
     }
